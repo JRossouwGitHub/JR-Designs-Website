136af7009c809b42191d6e0361039fd65d6fa35a
JRossouwGitHub <51179785+JRossouwGitHub@users.noreply.github.com>
2021-02-17 10:58:31 +1300

    Ran build command

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ mongoose
 app.use('/api/contacts', Contacts);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('build'));
+    app.use(express.static('dist'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
     });
 }
 
